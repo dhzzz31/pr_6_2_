7072e15c8ec3fa22d26e1699dbba77f6316e9a59
Seniy_Jalilow Seniy_Jalilow <arseniy-3108@mail.ru>
2023-11-08 22:23:09 +0500

    Добавьте файлы проекта.

---
+++ pr5_1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr5_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание значений из текстового поля textBox
+            double x = Convert.ToDouble(textBox1.Text);
+            double accuracy = Convert.ToDouble(textBox2.Text);
+
+            double sum = 0;
+            double slog = x;
+            int colslog = 1;
+            // Выполняеются расчеты суммы ряда, пока значение слагаемого больше или равно заданной точности
+            while (Math.Abs(slog) >= accuracy)
+            {
+                // Добавляем текущее слагаемое к общей сумме
+                sum += slog;
+                // Увеличиваем количество слагаемых на 1
+                colslog++;
+                // Вычисляем новое значение слагаемого
+                slog = x / colslog;
+            }
+           // Выводится результат
+            label4.Text = "sum = " + sum.ToString();
+            label5.Text = "quanity" + colslog.ToString();
+        }
+
+     
+    }
+}
